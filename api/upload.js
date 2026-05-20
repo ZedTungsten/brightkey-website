7d93e90d711c6bb299e5fa1824b24ed2f7443c8d
Zeus Taller <johnzeustaller@gmail.com>
2026-05-20 12:18:06 +0800

    feat: upload compiled receipt PDF directly to Bunny.net storage in the background

--- api/upload.js
+++ api/upload.js
@@ -42,6 +42,8 @@ export default async function handler(req, res) {
         folderPath = `installations/${safeRefId}/doors`;
       } else if (type === 'proof') {
         folderPath = `installations/${safeRefId}/proof`;
+      } else if (type === 'receipt') {
+        folderPath = `installations/${safeRefId}/receipt`;
       } else {
         folderPath = `installations/${safeRefId}`;
       }
