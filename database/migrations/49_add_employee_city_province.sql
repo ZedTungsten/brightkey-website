008a76cacd69aca3376c4e7a0edac83af9c71664
Zeus Taller <johnzeustaller@gmail.com>
2026-07-02 13:35:59 +0800

    revert: employee directory/registration database columns and parse city/province dynamically from employee address instead

--- database/migrations/49_add_employee_city_province.sql
+++
@@ -1,3 +0,0 @@
--- Migration: Add city and province columns to public.employees table
-ALTER TABLE public.employees ADD COLUMN IF NOT EXISTS city TEXT;
-ALTER TABLE public.employees ADD COLUMN IF NOT EXISTS province TEXT;
