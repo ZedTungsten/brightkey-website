fc2d0802bd33771e1fee468b4e80c2209585c8b1
Zeus Taller <johnzeustaller@gmail.com>
2026-07-17 04:24:24 +0800

    Update Google Drive icons to official 2020 SVG

--- dashboard/resources.js
+++ dashboard/resources.js
@@ -289,10 +289,13 @@ function renderExplorer() {
 
     const gdriveFolderBadgeHtml = isGDriveFolder ? `
       <div class="gdrive-badge" title="Google Drive Link">
-        <svg viewBox="0 0 100 100" width="100%" height="100%">
-          <polygon points="57.5,13.5 91,71.5 74.5,100 41,42" fill="#FFC107" />
-          <polygon points="25.5,71.5 74.5,71.5 91,100 9,100" fill="#34A853" />
-          <polygon points="41,42 9,100 25.5,71.5 57.5,13.5" fill="#1A73E8" />
+        <svg viewBox="0 0 87.3 78" width="100%" height="100%">
+          <path d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z" fill="#0066da"/>
+          <path d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z" fill="#00ac47"/>
+          <path d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z" fill="#ea4335"/>
+          <path d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z" fill="#00832d"/>
+          <path d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z" fill="#2684fc"/>
+          <path d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z" fill="#ffba00"/>
         </svg>
       </div>
     ` : '';
@@ -301,13 +304,13 @@ function renderExplorer() {
     const isGDrive = item.type !== 'folder' && (item.file_url && (item.file_url.includes('google.com') || item.file_url.includes('drive.google.com') || item.file_url.includes('docs.google.com')));
     const gdriveBadgeHtml = isGDrive ? `
       <div class="gdrive-badge" title="Google Drive Link">
-        <svg viewBox="0 0 100 100" width="100%" height="100%">
-          <!-- Yellow side (right) -->
-          <polygon points="57.5,13.5 91,71.5 74.5,100 41,42" fill="#FFC107" />
-          <!-- Green side (bottom) -->
-          <polygon points="25.5,71.5 74.5,71.5 91,100 9,100" fill="#34A853" />
-          <!-- Blue side (left) -->
-          <polygon points="41,42 9,100 25.5,71.5 57.5,13.5" fill="#1A73E8" />
+        <svg viewBox="0 0 87.3 78" width="100%" height="100%">
+          <path d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z" fill="#0066da"/>
+          <path d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z" fill="#00ac47"/>
+          <path d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z" fill="#ea4335"/>
+          <path d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z" fill="#00832d"/>
+          <path d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z" fill="#2684fc"/>
+          <path d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z" fill="#ffba00"/>
         </svg>
       </div>
     ` : '';
