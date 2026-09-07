a9460a6e831d6f0e9ca0708b668a31ca6345b532
Zeus Taller <johnzeustaller@gmail.com>
2026-09-07 19:28:01 +0800

    Update generated storefront assets

--- js/storefront-currency.generated.js
+++ js/storefront-currency.generated.js
@@ -1,1 +1,1 @@
-window.BKStorefrontCurrencies = Object.freeze({"e6cf43ed-1f42-4aad-a6ed-470147a0489f":"₱"});
+window.BKStorefrontCurrencies = Object.freeze({"e6cf43ed-1f42-4aad-a6ed-470147a0489f":"₱","bd36263c-5f69-40a8-9c72-c222f3e1e7a6":"₱"});
